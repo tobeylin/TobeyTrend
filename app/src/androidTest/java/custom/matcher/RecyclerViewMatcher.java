9e6def69a50a5fd4ec60995f64a18b14e32ea311
Tobey Lin <tobeylin@kkbox.com>
2015-07-24 14:04:20 +0800

    Update the RecyclerVIewMatcher.

--- app/src/androidTest/java/custom/matcher/RecyclerViewMatcher.java
+++ app/src/androidTest/java/custom/matcher/RecyclerViewMatcher.java
@@ -1,13 +1,13 @@
 package custom.matcher;
 
 import android.content.res.Resources;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
-import com.trend.tobeylin.tobeytrend.ui.custom.TypeTextView;
-
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.hamcrest.TypeSafeMatcher;
 
 /**
@@ -15,17 +15,75 @@ import org.hamcrest.TypeSafeMatcher;
  */
 public class RecyclerViewMatcher {
 
-    private final int recyclerViewId;
+    private RecyclerViewMatcher() {
 
-    public RecyclerViewMatcher(int recyclerViewId) {
-        this.recyclerViewId = recyclerViewId;
     }
 
-    public Matcher<View> atPosition(final int position) {
-        return atPositionOnView(position, -1);
+    public static Matcher<View> withRowCount(final int expectedRowCount) {
+        return withRowCount(Matchers.equalTo(expectedRowCount));
     }
 
-    public Matcher<View> atPositionOnView(final int position, final int targetViewId) {
+    public static Matcher<View> withRowCount(final Matcher<Integer> rowCountMatcher) {
+        return new TypeSafeMatcher<View>() {
+            @Override
+            protected boolean matchesSafely(View item) {
+                if (!(item instanceof RecyclerView)) {
+                    return false;
+                }
+                RecyclerView recyclerView = (RecyclerView) item;
+                RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+                if (!(layoutManager instanceof GridLayoutManager)) {
+                    return false;
+                }
+                GridLayoutManager gridLayoutManager = (GridLayoutManager) layoutManager;
+                int columnCount = gridLayoutManager.getSpanCount();
+                int itemCount = recyclerView.getChildCount();
+                int rowCount = itemCount / columnCount;
+                return rowCountMatcher.matches(rowCount);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("has row count: ");
+                rowCountMatcher.describeTo(description);
+            }
+        };
+    }
+
+    public static Matcher<View> withColumnCount(final int expectedColumnCount) {
+        return withColumnCount(Matchers.equalTo(expectedColumnCount));
+    }
+
+    public static Matcher<View> withColumnCount(final Matcher<Integer> columnCountMatcher) {
+        return new TypeSafeMatcher<View>() {
+            @Override
+            protected boolean matchesSafely(View item) {
+                if (!(item instanceof RecyclerView)) {
+                    return false;
+                }
+                RecyclerView recyclerView = (RecyclerView) item;
+                RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+                if (!(layoutManager instanceof GridLayoutManager)) {
+                    return false;
+                }
+                GridLayoutManager gridLayoutManager = (GridLayoutManager) layoutManager;
+                int columnCount = gridLayoutManager.getSpanCount();
+                return columnCountMatcher.matches(columnCount);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("has column count: ");
+                columnCountMatcher.describeTo(description);
+            }
+        };
+    }
+
+    public static Matcher<View> atPosition(final int recyclerViewId, final int position) {
+        return atPositionOnView(recyclerViewId, position, -1);
+    }
+
+    public static Matcher<View> atPositionOnView(final int recyclerViewId, final int position, final int targetViewId) {
 
         return new TypeSafeMatcher<View>() {
             Resources resources = null;
@@ -37,9 +95,7 @@ public class RecyclerViewMatcher {
                     try {
                         idDescription = this.resources.getResourceName(recyclerViewId);
                     } catch (Resources.NotFoundException var4) {
-                        idDescription = String.format("%s (resource name not found)",
-                                new Object[] { Integer.valueOf
-                                        (recyclerViewId) });
+                        idDescription = String.format("%s (resource name not found)", new Object[]{Integer.valueOf(recyclerViewId)});
                     }
                 }
 
@@ -51,12 +107,10 @@ public class RecyclerViewMatcher {
                 this.resources = view.getResources();
 
                 if (childView == null) {
-                    RecyclerView recyclerView =
-                            (RecyclerView) view.getRootView().findViewById(recyclerViewId);
+                    RecyclerView recyclerView = (RecyclerView) view.getRootView().findViewById(recyclerViewId);
                     if (recyclerView != null && recyclerView.getId() == recyclerViewId) {
                         childView = recyclerView.getChildAt(position);
-                    }
-                    else {
+                    } else {
                         return false;
                     }
                 }
